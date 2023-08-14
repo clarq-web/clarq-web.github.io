691c6f90c2e71a6f73c012641df4a00010a60eea
clarq <juniorclarksonoliveira>
2023-08-14 02:17:30 -0300

    Remoção de darkmode por preferência do sistema, houveram problemas com isso e preferi remover e adicionar um darkmode de fato, o padrão é o tema branco e o resto fica a critério do usuário.

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-let mqdm = window.matchMedia("(prefers-color-scheme: dark)");
+//checkbox-toggle
+const toggle = document.getElementById("mode-toggle");
+const nctc = document.querySelectorAll(".noncontac");
+const txt = document.querySelectorAll("h1, p, li, h4");
+const bckgrnd = document.querySelectorAll("header, section, footer");
 
-if (mqdm.matches) {
-  document.getElementById("mode-toggle").setAttribute("checked", "");
-}
+toggle.addEventListener("change", function () {
+  if (toggle.checked) {
+    nctc.forEach((element) => {
+      element.classList.add("txt-drk");
+    });
+    txt.forEach((element) => {
+      element.classList.add("txt-drk");
+    });
+    bckgrnd.forEach((element) => {
+      element.classList.add("bck-drk");
+    });
+  } else {
+    nctc.forEach((element) => {
+      element.classList.remove("txt-drk");
+    });
+    txt.forEach((element) => {
+      element.classList.remove("txt-drk");
+    });
+    bckgrnd.forEach((element) => {
+      element.classList.remove("bck-drk");
+    });
+  }
+});
