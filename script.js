e8ef2ea898a963870595e316c7c10bbda07cf34f
clarq <juniorclarksonoliveira>
2023-08-15 12:38:24 -0300

    Mudança no slogan.

--- script.js
+++ script.js
@@ -5,25 +5,25 @@ const txt = document.querySelectorAll("h1, p, li, h4");
 const bckgrnd = document.querySelectorAll("header, section, footer");
 
 toggle.addEventListener("change", function () {
-  if (toggle.checked) {
-    nctc.forEach((element) => {
-      element.classList.add("txt-drk");
-    });
-    txt.forEach((element) => {
-      element.classList.add("txt-drk");
-    });
-    bckgrnd.forEach((element) => {
-      element.classList.add("bck-drk");
-    });
-  } else {
-    nctc.forEach((element) => {
-      element.classList.remove("txt-drk");
-    });
-    txt.forEach((element) => {
-      element.classList.remove("txt-drk");
-    });
-    bckgrnd.forEach((element) => {
-      element.classList.remove("bck-drk");
-    });
-  }
+    if (toggle.checked) {
+      nctc.forEach((element) => {
+        element.classList.add("txt-drk");
+      });
+      txt.forEach((element) => {
+        element.classList.add("txt-drk");
+      });
+      bckgrnd.forEach((element) => {
+        element.classList.add("bck-drk");
+      });
+    } else {
+      nctc.forEach((element) => {
+        element.classList.remove("txt-drk");
+      });
+      txt.forEach((element) => {
+        element.classList.remove("txt-drk");
+      });
+      bckgrnd.forEach((element) => {
+        element.classList.remove("bck-drk");
+      });
+    }
 });
