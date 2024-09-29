ddb0a0c7957a5ae2298f71fe686b7d9f01a9ae59
rphldc <raphaelcamara.rc@gmail.com>
2024-09-29 12:40:37 -0300

    Atualizar estilos do rodapé e dos botões de redes sociais, mexendo no JS, CSS, e JavaScript

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
-//checkbox-toggle
+// Seleção de elementos
 const toggle = document.getElementById("mode-toggle");
-const nctc = document.querySelectorAll(".noncontac");
-const txt = document.querySelectorAll("h1, p, li, h4, h2");
-const bckgrnd = document.querySelectorAll("header, section, footer");
-const lg = document.querySelectorAll(".logo");
+const elementos = {
+  nctc: document.querySelectorAll(".noncontac"),
+  txt: document.querySelectorAll("h1, p, li, h4, h2"),
+  bckgrnd: document.querySelectorAll("header, section, footer"),
+  lg: document.querySelectorAll(".logo"),
+  socialButtons: document.querySelectorAll(".social-button")
+};
 
-toggle.addEventListener("change", function () {
-    if (toggle.checked) {
-      nctc.forEach((element) => {
-        element.classList.add("txt-drk");
-      });
-      txt.forEach((element) => {
-        element.classList.add("txt-drk");
-      });
-      bckgrnd.forEach((element) => {
-        element.classList.add("bck-drk");
-      });
-      lg.forEach((element) => {
-        element.innerHTML = "<img src=/media/wlogo.svg alt=logoheader>";
-      });
-    } else {
-      nctc.forEach((element) => {
-        element.classList.remove("txt-drk");
-      });
-      txt.forEach((element) => {
-        element.classList.remove("txt-drk");
-      });
-      bckgrnd.forEach((element) => {
-        element.classList.remove("bck-drk");
-      });
-      lg.forEach((element) => {
-        element.innerHTML = "";
-        element.innerHTML = "<img src=/media/blogo.svg alt=logoheader>";
-      });
-    }
+// Função para alternar classes
+function alternarClasses(elementos, acao, classe) {
+  elementos.forEach(elemento => elemento.classList[acao](classe));
+}
+
+// Função para alterar o logo
+function alterarLogo(elementos, src) {
+  elementos.forEach(elemento => {
+    elemento.innerHTML = `<img src=${src} alt="logoheader">`;
+  });
+}
+
+// Função para alternar o modo
+function alternarModo(ativado) {
+  const acaoClasse = ativado ? 'add' : 'remove';
+  const logoSrc = ativado ? '/media/wlogo.svg' : '/media/blogo.svg';
+
+  alternarClasses(elementos.nctc, acaoClasse, "txt-drk");
+  alternarClasses(elementos.txt, acaoClasse, "txt-drk");
+  alternarClasses(elementos.bckgrnd, acaoClasse, "bck-drk");
+  alternarClasses(elementos.socialButtons, acaoClasse, "social-button-dark");
+  alterarLogo(elementos.lg, logoSrc);
+}
+
+// Event listener para o toggle
+toggle.addEventListener("change", () => alternarModo(toggle.checked));
+
+// Event listener para os botões de redes sociais
+elementos.socialButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    window.location.href = button.getAttribute("data-href");
+  });
 });
